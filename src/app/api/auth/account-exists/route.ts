eaf5e6e2a379c436684c6a7c1ea9f473c46efd04
Joshua Epstein <18082@jfs.brent.sch.uk>
2024-09-28 00:26:29 +0100

    chore: Update Prisma import in account-exists route

--- src/app/api/auth/account-exists/route.ts
+++ src/app/api/auth/account-exists/route.ts
@@ -1,22 +1,20 @@
-import { prismaEdge } from '@logicate/database/edge';
-import { NextRequest, NextResponse } from 'next/server';
-
-export const runtime = 'edge';
+import { prisma } from '@logicate/database'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(req: NextRequest) {
-  const { email } = (await req.json()) as { email: string };
+  const { email } = (await req.json()) as { email: string }
 
-  const user = await prismaEdge.user.findUnique({
+  const user = await prisma.user.findUnique({
     where: { email },
-  });
+  })
 
   if (user) {
     return NextResponse.json({
       accountExists: true,
-    });
+    })
   }
 
   return NextResponse.json({
     accountExists: false,
-  });
+  })
 }
